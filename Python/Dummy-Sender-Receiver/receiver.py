70fc130ce97b26aa4a3d45129c24b277387d89de
Felix Weinrank <weinrank@fh-muenster.de>
2020-05-25 14:16:47 +0200

    sync with upstream

--- Python/Dummy-Sender-Receiver/receiver.py
+++ Python/Dummy-Sender-Receiver/receiver.py
@@ -6,8 +6,12 @@ import time
 
 config_mqtt_broker_ip = "10.42.10.86"
 config_mqtt_client_id = "dummy-receiver-" + str(random.randint(1000, 9999));
-config_mqtt_topic     = "sensor/#"
+config_mqtt_topic     = "sensor/3C:71:BF:AB:33:58"
+#config_mqtt_topic     = "sensor/3C:71:BF:AA:7B:18"
 
+
+
+ts_last_message = int(round(time.time() * 1000))
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -16,7 +20,11 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    #print(msg.topic + " " + str(msg.payload))
+    now = int(round(time.time() * 1000))
+    global ts_last_message
+    print(now - ts_last_message)
+    ts_last_message = now
 
 mqtt_c = mqtt.Client(config_mqtt_client_id)
 mqtt_c.on_connect = on_connect
